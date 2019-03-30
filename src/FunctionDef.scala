94f0cf5e15b08e75072ce1646c742d30641b7440
Bithan <bithandey89@gmail.com>
2019-03-30 22:55:36 +0530

    Create FunctionDef.scala

---
+++ src/FunctionDef.scala
@@ -0,0 +1,18 @@
+
+
+object FunctionDef {
+  
+  def add(x: Int,y: Int) : Int = {
+    return x + y;
+  }
+  
+  def sub(x: Int,y: Int): Int= {
+    return x-y
+  }
+      
+      
+  def main(args: Array[String]){
+   println(add(45,90))
+   println(sub(45,90))
+  }
+}
